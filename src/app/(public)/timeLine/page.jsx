1e1ef5bcef8b92b631f96115ba3fea581e94af32
amirulislambd <amirulislambd313@gmail.com>
2026-04-15 17:59:04 +0600

    added sorting system to history of timline page

--- src/app/(public)/timeLine/page.jsx
+++ src/app/(public)/timeLine/page.jsx
@@ -1,46 +1,72 @@
 "use client";
 import { frContext } from "@/context/FriendContext";
-import React, { useContext } from "react";
+import { notFound } from "next/navigation";
+import React, { useContext, useState } from "react";
 import { BiSolidPhoneCall } from "react-icons/bi";
-import { FaPhoneVolume, FaVideo } from "react-icons/fa";
 import { PiChatCircleTextLight } from "react-icons/pi";
 
 const TimeLinePage = () => {
   const { friendData, setFriendData } = useContext(frContext);
-  
-  return (
+
+  const [filterType, setFilterType] = useState("all");
+
+  const filteredData =
+    filterType === "all"
+      ? friendData
+      : friendData.filter((item) => item.type === filterType);
+
+  //   console.log(filterType);
+  return friendData.length > 0 ? (
     <div className="grid gap-2 max-w-7xl mx-auto my-5">
-      {friendData.map((friend) => (
-        <div
-          className="border p-2 md:p-4 border-zinc-200 rounded-xl flex items-center md:gap-4 mx-2 lg:mx-0"
-          key={friend.id}
+      <div className="mx-2 lg:mx-0">
+        <select
+          onChange={(e) => setFilterType(e.target.value)}
+          className="border border-zinc-200 w-full select outline-none max-w-xs p-2 rounded-md"
         >
-          <div>
-            {friend.type === "audio" ? (
-              <BiSolidPhoneCall className="text-2xl md:text-4xl" />
-            ) : friend.type === "text" ? (
-              <PiChatCircleTextLight className="text-2xl md:text-4xl" />
-            ) : friend.type === "video" ? (
-              <FaVideo className="text-2xl md:text-4xl" />
-            ) : (
-              ""
-            )}
-          </div>
-          <div>
-            <p className="flex gap-1">
-              <span className="font-bold">{friend.type} </span>
-              <span>with</span> 
-              <span>{friend?.friend?.name}</span>
-            </p>
-            <p> {new Date(friend.timestamp).toLocaleDateString('en-US',{
-                month: "long",
-                day: "numeric",
-                year: "numeric",
-            })}</p>
-          </div>
-        </div>
-      ))}
+          <option value="all">Filter Timeline</option>
+          <option value="audio">Audio Calls</option>
+          <option value="video">Video Calls</option>
+          <option value="text">Text Messages</option>
+        </select>
+      </div>
+      {filteredData.length > 0
+        ? filteredData.map((friend) => (
+            <div
+              className="border p-2 md:p-4 border-zinc-200 rounded-xl flex items-center md:gap-4 mx-2 lg:mx-0"
+              key={friend.id}
+            >
+              <div>
+                {friend.type === "audio" ? (
+                  <BiSolidPhoneCall className="text-2xl md:text-4xl" />
+                ) : friend.type === "text" ? (
+                  <PiChatCircleTextLight className="text-2xl md:text-4xl" />
+                ) : friend.type === "video" ? (
+                  <FaVideo className="text-2xl md:text-4xl" />
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                <p className="flex gap-1">
+                  <span className="font-bold">{friend.type} </span>
+                  <span>with</span>
+                  <span>{friend?.friend?.name}</span>
+                </p>
+                <p>
+                  {" "}
+                  {new Date(friend.timestamp).toLocaleDateString("en-US", {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+            </div>
+          ))
+        : ""}
     </div>
+  ) : (
+    notFound()
   );
 };
 
